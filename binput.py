dd0b96eaca2351cc200ccdd8429321de17f35f37
Above the Book <58607747+AbovetheBook@users.noreply.github.com>
2020-06-02 18:46:14 -0400

    input | two buy side two sell side binary options

--- binput.py
+++ binput.py
@@ -10,18 +10,18 @@ contract = "AUD/USD"
 
 b1str = float(.6884)
 b1avgp = 81.25
-b1size = 40
+b1size = 10
 
 s1str = float(.6901)
 s1avgp = 18.25
-s1size = -.1
+s1size = -80
 
 #additional positions
 
 b2p = False
-b2str = float(0.66)
-b2avgp = 80.5
-b2size = 30
+b2str = float(.688)
+b2avgp = 70.5
+b2size = 60
 
 s2p = False
 s2str = float(0.84)
@@ -36,22 +36,26 @@ s1cost = abs(s1size) * (100-s1avgp)
 s1win = (abs(s1size)*s1avgp)
 
 b2cost = b2avgp * b2size
-b2win = 200*b2size - (b2cost)
+b2win = 100*b2size - (b2cost)
 s2cost = abs(s2size) * (100-s2avgp)
 s2win = (abs(s2size)*s2avgp)
 
 
-
+print()
 print("1: " + contract + " > " + str(b1str) + " | size: " + str(b1size) + " | risk: $" + str(b1cost) + " | net: $" + str(b1win))
 
-if b2p == True:
-	print(contract + " > " + str(b2str) + " | size: " + str(b2size) + " | risk: $" + str(b2cost) + " | net: $" + str(b2win))
 
 
 print("2: " + contract + " > " + str(s1str) + " | size: " + str(s1size) + " | risk: $" + str(s1cost) + " | net: $" + str(s1win))
 
-if s2p == True:
-	print(contract + " > " + str(s2str) + " | size: " + str(s2size) + " | risk: $" + str(s2cost) + " | net: $" + str(s2win))
+if b2p == True:
+	print("3: " + contract + " > " + str(b2str) + " | size: " + str(b2size) + " | risk: $" + str(b2cost) + " | net: $" + str(b2win))
+
+
+if s2p == True and b2p == True:
+	print("4: " + contract + " > " + str(s2str) + " | size: " + str(s2size) + " | risk: $" + str(s2cost) + " | net: $" + str(s2win))
+elif s2p == True and b2p == False:
+	print("3: " + contract + " > " + str(s2str) + " | size: " + str(s2size) + " | risk: $" + str(s2cost) + " | net: $" + str(s2win))
 
 print()
 
