70d508e0fd9e3fc46742fa7f37785f00e4bb3543
Above the Book <58607747+AbovetheBook@users.noreply.github.com>
2020-06-02 18:37:39 -0400

    analyze 2 or 3 binary options

--- analyze_binary.py
+++ analyze_binary.py
@@ -6,15 +6,17 @@ b2p, b2cost, b2win, s2p, s2cost, s2win = bi.b2p, bi.b2cost, bi.b2win, bi.s2p, bi
 b2str, b2avgp, b2size, s2str, s2avgp, s2size = bi.b2str, bi.b2avgp, bi.b2size, bi.s2str, bi.s2avgp, bi.s2size
 contract = bi.contract
 
+#risk sum for og buy / sell side orders
 tcost1 = b1cost + s1cost
 
+#calculate avg price if there are two buy side orders
 bcostsum = b1cost + b2cost
 bcost = b1cost
+#b2p: is bet 2 placed? variable
 if b2p == True:
 	bcost = bcostsum
 	bavgp = (b1cost + b2cost) / (b1size + b2size)
-else:
-	bavgp = b1avgp
+else: bavgp = b1avgp
 
 scostsum = s1cost + s2cost
 scost = s1cost
@@ -25,31 +27,67 @@ if s2p == True:
 tcost = round(scost + bcost, ndigits=2)
 
 
-ww1 = round((b1win+s1win),ndigits=2)
-wL1 = round((b1win-s1cost),ndigits=2)
-Lw1 = round((s1win-b1cost),ndigits=2)
 
-#title variables for win and loss scenarios
-b1wT = contract + ">" + str(b1str)
-b1LT = contract + "<" + str(b1str)
-s1wT = contract + "<" + str(s1str)
-s1LT = contract + ">" + str(s1str)
+#b2p == True
+
+www = b1win + s1win + b2win
+wwL = b1win + s1win - b2cost
+Lww = -b1cost + s1win + b2win
+wLw = b1win - s1cost + b2win
+LwL = -b1cost + s1win - b2cost
 
 
-print("win-win: $", str(ww1), " | ", b1wT, " | ", s1wT)
-print("win-loss: $", str(wL1), " | ", b1wT, " | ", s1LT)
-print("loss-win: $", str(Lw1), " | ", b1LT, " | ", s1wT)
+if b2p == True and s2p == False:
 
-print()
+	b1H = False
+	if b1str > b2str: b1H = True
 
-#ROI
 
-roiww = round(((ww1/tcost1)*100),ndigits=2)
-roiwL = round(((wL1/tcost1)*100),ndigits=2)
-roiLw = round(((Lw1/tcost1)*100),ndigits=2)
+	b1wT = contract + ">" + str(b1str)
+	b2wT = contract + ">" + str(b2str)
+	s1wT = contract + "<" + str(s1str)
+	b1LT = contract + "<" + str(b1str)
+	b2LT = contract + "<" + str(b2str)
+	s1LT = contract + ">" + str(s1str)
 
-print("ROI win-win: ", roiww, "%")
-print("ROI win-loss: ", roiwL, "%")
-print("ROI loss-win: ", roiLw, "%")
+	if b1H == True:
+
+		print("www: $", www, b1wT, " | ", s1wT, " | ", b2wT)
+		print("Lww: $", Lww, b1LT, " | ", s1wT, " | ", b2wT)
+		print("wLw: $", wLw, b1wT, " | ", s1LT, " | ", b2wT)
+		print("LwL: $", LwL, b1LT, " | ", s1wT, " | ", b2LT)
+
+	elif b1H == False:
+
+		print("www: $", www, b1wT, " | ", s1wT, " | ", b2wT)
+		print("wwL: $", wwL, b1wT, " | ", s1wT, " | ", b2LT)
+		print("wLw: $", wLw, b1wT, " | ", s1LT, " | ", b2wT)
+		print("LwL: $", LwL, b1LT, " | ", s1wT, " | ", b2LT)
+
+
+
+
+
+
+
+
+if b2p == False and s2p == False:
+	ww1 = round((b1win+s1win),ndigits=2)
+	wL1 = round((b1win-s1cost),ndigits=2)
+	Lw1 = round((s1win-b1cost),ndigits=2)
+
+	#title variables for win and loss scenarios
+	b1wT = contract + ">" + str(b1str)
+	b1LT = contract + "<" + str(b1str)
+	s1wT = contract + "<" + str(s1str)
+	s1LT = contract + ">" + str(s1str)
+	roiww = round(((ww1/tcost1)*100),ndigits=2)
+	roiwL = round(((wL1/tcost1)*100),ndigits=2)
+	roiLw = round(((Lw1/tcost1)*100),ndigits=2)
+
+	print("win-win: $", str(ww1), " | ROI:", roiww, "%" " | ", b1wT, " | ", s1wT)
+	print("win-loss: $", str(wL1), " | ROI:", roiwL, "%", " | ", b1wT, " | ", s1LT)
+	print("loss-win: $", str(Lw1), " | ROI:", roiLw, "%", " | ", b1LT, " | ", s1wT)
+
 
 print()
