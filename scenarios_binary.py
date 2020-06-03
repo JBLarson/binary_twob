8fdfc8545562e4f3b49ccdc4acc94ced45d3308b
Above the Book <58607747+AbovetheBook@users.noreply.github.com>
2020-06-03 15:17:36 -0400

    analyze binary option pairings

--- scenarios_binary.py
+++ scenarios_binary.py
@@ -2,7 +2,7 @@ from pandas import DataFrame
 import pandas as pd
 
 
-import binput as bi
+import binput2 as bi
 
 contract, b1c, b1w, s1c, s1w = bi.contract, bi.b1c, bi.b1w, bi.s1c, bi.s1w
 b1str, b1avgp, b1size, s1str, s1avgp, s1size = bi.b1str, bi.b1avgp, bi.b1size, bi.s1str, bi.s1avgp, bi.s1size
@@ -12,7 +12,7 @@ b2str, b2avgp, b2size, s2str, s2avgp, s2size = bi.b2str, bi.b2avgp, bi.b2size, b
 #risk sum for og buy / sell side orders
 tcost, tcost1, tcost2 = b1c + s1c, b1c + b2c + s1c, b1c + s1c + s2c
 
-contract = ''
+#contract = ''
 
 b1wT, b2wT = contract + ">" + str(b1str), contract + ">" + str(b2str)
 b1LT, b2LT = contract + "<" + str(b1str), contract + "<" + str(b2str)
@@ -108,129 +108,69 @@ if b2p == False and s2p == False:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-
-legend = []
-t1x = []
-t2x = []
-t3x = []
-t4x = []
-t5x = []
-
-legend.append("Over:")
-legend.append("Under:")
-legend.append("b1risk")
-legend.append("b1win")
-legend.append("b2risk")
-legend.append("b2win")
-legend.append("bcs")
-legend.append("winloss")
-legend.append("losswin")
-
-#append variables to lists
-
-for n in ['1','2', '3', '4', '5']:
-	for s in ['b1s','b2s',f'adj.b1r_t{n}',f'b1w_t{n}',f'adj.b2r_t{n}',f'b2w_t{n}',f'bcs_t{n}',f'winloss_t{n}',f'losswin_t{n}']:
-		eval(f't{n}x').append(eval(s))
-
-trialz = pd.DataFrame({'key': legend, 't1': t1x, 't2': t2x, 't3': t3x, 't4': t4x, 't5': t5x})
-
-
-"""
-
-
-
-
-
-
-
-
-
 #two buy's one sell
 if b2p == True and s2p == False:
 
 	www, wwL, Lww = b1w + s1w + b2w, b1w + s1w - b2c, -b1c + s1w + b2w
-	wLw, LwL = b1w - s1c + b2w, -b1c + s1w - b2c
-
-	b1H = False
-	if b1str > b2str: b1H = True
-
-	if b1H == True: #strike price of 1st buy > 2nd buy
-		roiwww, roiLww = round(((www/tcost2)*100),ndigits=2), round(((Lww/tcost2)*100),ndigits=2)
-		roiwLw, roiLwL = round(((wLw/tcost2)*100),ndigits=2), round(((LwL/tcost2)*100),ndigits=2)
-
-		key = []
-
-		key.append("strike")
-		key.append("size")
-		key.append("price")
-		key.append("cost")
-		key.append("win")
-
-		b1x, b2x, s1x = [], [], []
-
-		for n in ['b1','s1', 'b2']:
-			for s in [f'{n}str',f'{n}size',f'{n}avgp',f'{n}c',f'{n}w']:
-				eval(f'{n}x').append(eval(s))
+	wLw, LwL, key = b1w - s1c + b2w, -b1c + s1w - b2c, []
 
 
-		trialz = pd.DataFrame({'key': key, 'b1': b1x, 's1': s1x, 'b2': b2x})
+	key.append("strike")
+	key.append("size")
+	key.append("price")
+	key.append("cost")
+	key.append("win")
 
-		trialz['avg'] = round((trialz['b1'] + trialz['b2'] + trialz['s1'])/3,ndigits=2)
+	b1x, b2x, s1x = [], [], []
 
-		avgcost = (trialz['avg'][3])
+	for n in ['b1','s1', 'b2']:
+		for s in [f'{n}str',f'{n}size',f'{n}avgp',f'{n}c',f'{n}w']:
+			eval(f'{n}x').append(eval(s))
+
+
+	b2s1 = pd.DataFrame({'key': key, 'b1': b1x, 's1': s1x, 'b2': b2x})
+
+	b2s1['avg'] = round((b2s1['b1'] + b2s1['b2'] + b2s1['s1'])/3,ndigits=2)
+
+	b2s1avgc = (b2s1['avg'][3])
 
 
 
-		print()
+	print()
 
-		print(trialz)
-
-		ss = www + Lww + wLw + LwL
-
-	elif b1H == False:
-
-		roiwww, roiwwL = round(((www/tcost2)*100),ndigits=2), round(((wwL/tcost2)*100),ndigits=2)
-		roiwLw, roiLwL = round(((wLw/tcost2)*100),ndigits=2), round(((LwL/tcost2)*100),ndigits=2)
-		ss = www + wwL + wLw + LwL
+	print(b2s1)
 
 
 
-if b2p == False and s2p == True:
+elif b2p == False and s2p == True:
 
 	www, wwL, Lww = s1w + b1w + s2w, s1w + b1w - s2c, -s1c + b1w + s2w
-	wLw, LwL = s1w - b1c + s2w, -s1c + b1w - s2c
+	wLw, LwL, key = s1w - b1c + s2w, -s1c + b1w - s2c, []
+
+	key.append("strike")
+	key.append("size")
+	key.append("price")
+	key.append("cost")
+	key.append("win")
+
+	b1x, s2x, s1x = [], [], []
+
+	for n in ['b1','s1', 's2']:
+		for s in [f'{n}str',f'{n}size',f'{n}avgp',f'{n}c',f'{n}w']:
+			eval(f'{n}x').append(eval(s))
 
 
-	s1H = False
-	if s1str > s2str: s1H = True
+	s2b1 = pd.DataFrame({'key': key, 'b1': b1x, 's1': s1x, 's2': s2x})
 
-	if s1H == True: #strike price of 1st buy > 2nd buy
+	s2b1['avg'] = round((s2b1['b1'] + s2b1['s1'] + s2b1['s2'])/3,ndigits=2)
 
-		roiwww, roiwwL = round(((www/tcost2)*100),ndigits=2), round(((wwL/tcost2)*100),ndigits=2)
-		roiwLw, roiLwL = round(((wLw/tcost2)*100),ndigits=2), round(((LwL/tcost2)*100),ndigits=2)
+	avgcost = (s2b1['avg'][3])
 
-		ss = www + wwL + wLw + LwL
 
-	elif s1H == False:
-		roiwww, roiLww = round(((www/tcost2)*100),ndigits=2), round(((Lww/tcost2)*100),ndigits=2)
-		roiwLw, roiLwL = round(((wLw/tcost2)*100),ndigits=2), round(((LwL/tcost2)*100),ndigits=2)
 
-		ss = www + Lww + wLw + LwL
+	print()
 
+	print(s2b1)
 
 
 if b2p == False and s2p == False:
