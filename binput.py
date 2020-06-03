0d218be34c288627a02b196df8d5f6351c28a0c9
Above the Book <58607747+AbovetheBook@users.noreply.github.com>
2020-06-03 15:19:49 -0400

    input for 2-4 binary options

--- binput.py
+++ binput.py
@@ -4,58 +4,61 @@ import numpy as np
 
 #the event the two options relate to
 #contract = "USD/JPY>108"
-contract = "AUD/USD"
+#contract = "AUD/USD"
+
+contract = "USD/JPY"
+
 
 #b1str is the buy side strike price
 
-b1str = float(.6884)
-b1avgp = 81.25
-b1size = 10
+b1str = float(108.58)
+b1avgp = 74.15
+b1size = 100
 
-s1str = float(.6901)
-s1avgp = 18.25
-s1size = -80
+s1str = float(108.74)
+s1avgp = 29.02
+s1size = -238
 
 #additional positions
 
 b2p = False
-b2str = float(.688)
-b2avgp = 70.5
-b2size = 60
+b2str = float(108.56)
+b2avgp = 67
+b2size = 100
 
-s2p = False
-s2str = float(0.84)
-s2avgp = 36.5
-s2size = -400
+s2p = True
+s2str = float(108.66)
+s2avgp = 33.75
+s2size = -50
 
 
 
-b1cost = b1avgp * b1size
-b1win = 100*b1size - (b1cost)
-s1cost = abs(s1size) * (100-s1avgp)
-s1win = (abs(s1size)*s1avgp)
+b1c = b1avgp * b1size
+b1w = 100*b1size - (b1c)
+s1c = abs(s1size) * (100-s1avgp)
+s1w = (abs(s1size)*s1avgp)
 
-b2cost = b2avgp * b2size
-b2win = 100*b2size - (b2cost)
-s2cost = abs(s2size) * (100-s2avgp)
-s2win = (abs(s2size)*s2avgp)
+b2c = b2avgp * b2size
+b2w = 100*b2size - (b2c)
+s2c = abs(s2size) * (100-s2avgp)
+s2w = (abs(s2size)*s2avgp)
 
 
 print()
-print("1: " + contract + " > " + str(b1str) + " | size: " + str(b1size) + " | risk: $" + str(b1cost) + " | net: $" + str(b1win))
+print("1: " + contract + " > " + str(b1str) + " | size: " + str(b1size) + " | risk: $" + str(b1c) + " | net: $" + str(b1w))
 
 
 
-print("2: " + contract + " > " + str(s1str) + " | size: " + str(s1size) + " | risk: $" + str(s1cost) + " | net: $" + str(s1win))
+print("2: " + contract + " > " + str(s1str) + " | size: " + str(s1size) + " | risk: $" + str(s1c) + " | net: $" + str(s1w))
 
 if b2p == True:
-	print("3: " + contract + " > " + str(b2str) + " | size: " + str(b2size) + " | risk: $" + str(b2cost) + " | net: $" + str(b2win))
+	print("3: " + contract + " > " + str(b2str) + " | size: " + str(b2size) + " | risk: $" + str(b2c) + " | net: $" + str(b2w))
 
 
 if s2p == True and b2p == True:
-	print("4: " + contract + " > " + str(s2str) + " | size: " + str(s2size) + " | risk: $" + str(s2cost) + " | net: $" + str(s2win))
+	print("4: " + contract + " > " + str(s2str) + " | size: " + str(s2size) + " | risk: $" + str(s2c) + " | net: $" + str(s2w))
 elif s2p == True and b2p == False:
-	print("3: " + contract + " > " + str(s2str) + " | size: " + str(s2size) + " | risk: $" + str(s2cost) + " | net: $" + str(s2win))
+	print("3: " + contract + " > " + str(s2str) + " | size: " + str(s2size) + " | risk: $" + str(s2c) + " | net: $" + str(s2w))
 
 print()
 
