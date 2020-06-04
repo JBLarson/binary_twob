411027eb6039c87b4c3bfbd532a13a0131f3ebeb
Above the Book <58607747+AbovetheBook@users.noreply.github.com>
2020-06-04 18:43:39 -0400

    binary option inputs

--- binput.py
+++ binput.py
@@ -2,63 +2,25 @@ from pandas import DataFrame
 import pandas as pd
 import numpy as np
 
-#the event the two options relate to
-#contract = "USD/JPY>108"
 #contract = "AUD/USD"
-
 contract = "USD/JPY"
-
 
 #b1str is the buy side strike price
 
-b1str = float(108.58)
-b1avgp = 74.15
-b1size = 100
+b1str = float(108.90)
+b1avgp = 70
+b1size = 150
 
-s1str = float(108.74)
-s1avgp = 29.02
-s1size = -238
+s1str = float(108.98)
+s1avgp = 42
+s1size = 200
 
 #additional positions
 
-b2p = False
-b2str = float(108.56)
-b2avgp = 67
-b2size = 100
+b2p, b2str, b2avgp, b2size = False, float(108.94), 51, 100
 
-s2p = True
-s2str = float(108.66)
-s2avgp = 33.75
-s2size = -50
+s2p, s2str, s2avgp, s2size = False, float(109.24), 65, 45
 
-
-
-b1c = b1avgp * b1size
-b1w = 100*b1size - (b1c)
-s1c = abs(s1size) * (100-s1avgp)
-s1w = (abs(s1size)*s1avgp)
-
-b2c = b2avgp * b2size
-b2w = 100*b2size - (b2c)
-s2c = abs(s2size) * (100-s2avgp)
-s2w = (abs(s2size)*s2avgp)
-
-
-print()
-print("1: " + contract + " > " + str(b1str) + " | size: " + str(b1size) + " | risk: $" + str(b1c) + " | net: $" + str(b1w))
-
-
-
-print("2: " + contract + " > " + str(s1str) + " | size: " + str(s1size) + " | risk: $" + str(s1c) + " | net: $" + str(s1w))
-
-if b2p == True:
-	print("3: " + contract + " > " + str(b2str) + " | size: " + str(b2size) + " | risk: $" + str(b2c) + " | net: $" + str(b2w))
-
-
-if s2p == True and b2p == True:
-	print("4: " + contract + " > " + str(s2str) + " | size: " + str(s2size) + " | risk: $" + str(s2c) + " | net: $" + str(s2w))
-elif s2p == True and b2p == False:
-	print("3: " + contract + " > " + str(s2str) + " | size: " + str(s2size) + " | risk: $" + str(s2c) + " | net: $" + str(s2w))
-
-print()
-
+b1c, b2c = (b1avgp * b1size), (b2avgp * b2size)
+b1w, s1c, s1w = 100*b1size - (b1c), abs(s1size) * (100-s1avgp), (abs(s1size)*s1avgp)
+b2w, s2c, s2w = 100*b2size - (b2c), abs(s2size) * (100-s2avgp), (abs(s2size)*s2avgp)
